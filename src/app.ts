e7237befea5d6c020f2305b185fd5b1200eece87
ByteByNabil <k.nabil.cse@gmail.com>
2026-04-24 20:07:05 +0600

    better-auth set

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,18 @@
 import express, { Application } from "express";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
+
 
 app.use(cors({ origin: process.env.APP_URL, credentials: true }))
 app.use(express.json())
 
+app.all("/api/auth/*splat", toNodeHandler(auth))
+
 app.get("/", (req, res) => {
     res.send("FoodHub API running...");
 });
-
-
 
 export default app;
